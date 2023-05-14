64d695325cbb69105ebae3c123ddf7a4ffced782
Oakmoater <dubrovecva@gmail.com>
2023-05-14 21:38:50 +0200

    almost done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from "react";
+import Searchbar from "./Searchbar/Searchbar";
+import { getImages } from "API/getSearchImages";
+import ImageGallery from "./ImageGallery/ImageGallery";
+
+
+export class App extends Component {
+  state = {
+    galleryItems: [],
+    query: ''
+  }
+
+  searchHandler = (e) => {
+    e.preventDefault();
+    const { input } = e.target.elements
+    this.setState({ query: input.value })
+    getImages(input.value, 1)
+      .then(({ hits }) => {
+        this.setState({ galleryItems: hits });
+    })
+    .catch(er => console.log(er));
+  }
+
+  loadMoreHandler = () => {
+    getImages(this.state.query, this.state.galleryItems.length / 12 + 1)
+      .then(({ hits }) => {
+        this.setState(prevState => ({
+          galleryItems: [...prevState.galleryItems, ...hits]
+        }));
+      })
+    .catch(er => console.log(er));
+  }
+
+  render()  {
+    return (
+      <div>
+        <Searchbar onSubmit={this.searchHandler} />
+        <ImageGallery Images={this.state.galleryItems} loadMoreHandler={this.loadMoreHandler} />
+      </div>
+    );
+  }
 };
